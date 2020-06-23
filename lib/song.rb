39ef78fb37e70476572a3ddd2bc128b55de85d4d
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-23 21:21:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @artist = artist 
     @genre = genre 
     @@count += 1
-    @@genres << genre unless @@genre.include?(genre)
+    @@genres << genre unless @@genres.include?(genre)
   end
   
   def self.count 
